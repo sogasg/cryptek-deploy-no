a5bdec9e769e278eadcbd6c8d4d0d00fae3615f3
Asgeir Sognefest <sogasg@gmail.com>
2019-02-21 12:28:33 +0100

    Updates

---
+++ precache-manifest.a74c21dfb07c1d1a29e600493b744da0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "301f931460a1b46d979d",
+    "url": "/static/js/main.301f9314.chunk.js"
+  },
+  {
+    "revision": "850eec8331d802e72905",
+    "url": "/static/js/1.850eec83.chunk.js"
+  },
+  {
+    "revision": "fc0112819bef4e4aea02ae024d865b29",
+    "url": "/index.html"
+  }
+];
